83ce744b2a7be276d1f711209f153e677b9f7ee1
Minwoo <mwkim0919@gmail.com>
2017-07-30 18:14:51 -0700

    added dynamic filtering functionality in FilteringController (has some bugs)

--- src/main/java/com/mwkim/restwebservices/controllers/FilteringController.java
+++ src/main/java/com/mwkim/restwebservices/controllers/FilteringController.java
@@ -1,6 +1,10 @@
 package com.mwkim.restwebservices.controllers;
 
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 import com.mwkim.restwebservices.models.SomeBean;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,13 +14,29 @@ import java.util.List;
 @RestController
 public class FilteringController {
 
+    // only send field1 and field2
     @GetMapping("/filtering")
     public SomeBean retrieveSomeBean() {
-        return new SomeBean("value1", "value2", "value3");
+        SomeBean someBean = new SomeBean("value1", "value2", "value3");
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field1", "field2");
+        FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+        MappingJacksonValue mapping = new MappingJacksonValue(someBean);
+        mapping.setFilters(filters);
+
+        return someBean;
     }
 
+    // only send field2 and field3
     @GetMapping("/filtering-list")
     public List<SomeBean> retrieveListOfSomeBean() {
-        return Arrays.asList(new SomeBean("value1", "value2", "value3"), new SomeBean("v1", "v2", "v3"));
+        List<SomeBean> someBeans = Arrays.asList(new SomeBean("value1", "value2", "value3"), new SomeBean("v1", "v2", "v3"));
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field2", "field3");
+        FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+        MappingJacksonValue mapping = new MappingJacksonValue(someBeans);
+        mapping.setFilters(filters);
+
+        return someBeans;
     }
 }
