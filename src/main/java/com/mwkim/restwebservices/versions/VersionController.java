702795eaba533676844f505445c5c349cdd5fc10
Minwoo <mwkim0919@gmail.com>
2017-07-30 18:49:12 -0700

    added basic authentication for security purpose

--- src/main/java/com/mwkim/restwebservices/versions/VersionController.java
+++ src/main/java/com/mwkim/restwebservices/versions/VersionController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class VersionController {
 
+    // URI VERSIONING
     @GetMapping("/v1/person")
     public PersonV1 personV1() {
         return new PersonV1("Bob Charlie");
@@ -16,5 +17,36 @@ public class VersionController {
         return new PersonV2("Minwoo", "Kim");
     }
 
+    // PARAM VERSIONING
+    @GetMapping(value="/person/param", params="version=1")
+    public PersonV1 paramV1() {
+        return new PersonV1("Bob Charlie");
+    }
 
+    @GetMapping(value="/person/param", params="version=2")
+    public PersonV2 paramV2() {
+        return new PersonV2("Minwoo", "Kim");
+    }
+
+    // HEADER VERSIONING
+    @GetMapping(value="/person/header", headers="X-API-VERSION=1")
+    public PersonV1 headerV1() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(value="/person/header", headers="X-API-VERSION=2")
+    public PersonV2 headerV2() {
+        return new PersonV2("Minwoo", "Kim");
+    }
+
+    // COMMON VERSIONING METHOD: CONTENT NEGOTIATION OR ACCEPT HEADER VERSIONING
+    @GetMapping(value="/person/produces", produces="application/vnd.company.app-v1+json")
+    public PersonV1 produceV1() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(value="/person/produces", produces="application/vnd.company.app-v2+json")
+    public PersonV2 produceV2() {
+        return new PersonV2("Minwoo", "Kim");
+    }
 }
